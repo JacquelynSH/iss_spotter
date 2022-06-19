1ec807e4fa97a85b158c5a7905a14e0d1c985073
JacquelynSH <elpatchaembroidery@gmail.com>
2022-06-19 16:08:41 -0700

    add fetchCoordsByIP function to find coordinates based on ip

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js will require and run out main fetch functions
+
+
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned IP:' , ip);
+});
+
+
+// fetchCoordsByIP('24.80.37.213', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
---
+++ iss.js
@@ -0,0 +1,37 @@
+// iss.js will contain most of the logic for fetching the data from each API endpoint
+
+const request = require("request");
+
+const fetchMyIP = function(callback) {
+  request(`https://api.ipify.org?format=json`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    } if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  //API key was required to get the actual correct answer , but was advised by mentor to not commit with my key.
+  request(`https://api.ipbase.com/json/?apikey=${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
