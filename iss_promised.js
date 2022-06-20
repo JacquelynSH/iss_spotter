6baf9d56eb52192f23f80bd482b887f9a920421e
JacquelynSH <elpatchaembroidery@gmail.com>
2022-06-20 12:26:24 -0700

    add nextISSTimesForMyLocation function

--- iss_promised.js
+++ iss_promised.js
@@ -1,13 +1,30 @@
 const request = require('request-promise-native');
 
 const fetchMyIP = function() {
-return request('https://api.ipify.org?format=json');
+  return request('https://api.ipify.org?format=json');
 
 };
 
 const fetchCoordsByIP = function(body) {
   const ip = JSON.parse(body).ip;
-  return request(`https://freegeoip.app/json/${ip}`);
+  return request(`https://api.ipbase.com/json/?apikey=U9dbuq784KcNGlhWU4c3tOLr9xqjht7dT3rFE9Im`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`
+  return request(url)
+
 }
 
-module.exports = { fetchMyIP, fetchCoordsByIP};
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then((data) => {
+    const { response } = JSON.parse(data);
+    return response
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation };
